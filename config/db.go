9cfb24d47ca0ad06c1710fc6faf6a09b57842daf
insanXYZ <insan.nazal@gmail.com>
2024-11-27 17:25:41 +0700

    chore: tiny changes of env

--- config/db.go
+++ config/db.go
@@ -2,12 +2,15 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func NewDb() *sql.DB {
-	sql, err := sql.Open("mysql", "root:@tcp(localhost:3306)/rad?parseTime=true")
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_TABLE"))
+	sql, err := sql.Open("mysql", dataSourceName)
 	if err != nil {
 		panic(err.Error())
 	}
--- config/echo.go
+++ config/echo.go
@@ -11,9 +11,7 @@ type TemplateRenderer struct {
 	templates *template.Template
 }
 
-// Render renders a template document
 func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	// Add global methods if data is a map
 	if viewContext, isMap := data.(map[string]interface{}); isMap {
 		viewContext["reverse"] = c.Echo().Reverse
 	}
